665af115918fe8fabcddd677a3a449149ab2dfd2
Saru <saru2020@gmail.com>
2020-08-31 14:01:29 +0530

    Movie: genre changes in mock data

--- app/data/Movies.ts
+++ app/data/Movies.ts
@@ -9,7 +9,7 @@ const Movies: Array<Movie> = [
         name: 'Aladdin',
         poster:
             'https://raw.githubusercontent.com/enahum/mattermost-react-native-interview/master/posters/Aladdin.jpg',
-        gender: 'Family',
+        genre: 'Family',
         description:
             'A kind-hearted street urchin and a power-hungry Grand Vizier vie for a magic lamp that has the power to make their deepest wishes come true.',
     },
@@ -17,7 +17,7 @@ const Movies: Array<Movie> = [
         name: 'Aquaman',
         poster:
             'https://raw.githubusercontent.com/enahum/mattermost-react-native-interview/master/posters/Aquaman.jpg',
-        gender: 'Adventure',
+        genre: 'Adventure',
         description:
             'Arthur Curry (Jason Momoa), the human-born heir to the underwater kingdom of Atlantis, goes on a quest to prevent a war between the worlds of ocean and land.',
     },
@@ -25,7 +25,7 @@ const Movies: Array<Movie> = [
         name: 'Avatar',
         poster:
             'https://raw.githubusercontent.com/enahum/mattermost-react-native-interview/master/posters/Avatar.jpg',
-        gender: 'Adventure',
+        genre: 'Adventure',
         description:
             'A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.',
     },
@@ -33,7 +33,7 @@ const Movies: Array<Movie> = [
         name: 'Beauty and the beast',
         poster:
             'https://raw.githubusercontent.com/enahum/mattermost-react-native-interview/master/posters/Beauty-Beast.jpg',
-        gender: 'Fantasy',
+        genre: 'Fantasy',
         description:
             'A selfish Prince is cursed to become a monster for the rest of his life, unless he learns to fall in love with a beautiful young woman he keeps prisoner.',
     },
@@ -41,7 +41,7 @@ const Movies: Array<Movie> = [
         name: 'Black Widow',
         poster:
             'https://raw.githubusercontent.com/enahum/mattermost-react-native-interview/master/posters/Black-Widow.jpg',
-        gender: 'Action',
+        genre: 'Action',
         description:
             'A film about Natasha Romanoff in her quests between the films Civil War and Infinity War.',
     },
@@ -49,7 +49,7 @@ const Movies: Array<Movie> = [
         name: 'Blade Runner',
         poster:
             'https://raw.githubusercontent.com/enahum/mattermost-react-native-interview/master/posters/Blade-Runner.jpg',
-        gender: 'Sci-Fi',
+        genre: 'Sci-Fi',
         description:
             'A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.',
     },
@@ -57,7 +57,7 @@ const Movies: Array<Movie> = [
         name: 'Bloodshot',
         poster:
             'https://raw.githubusercontent.com/enahum/mattermost-react-native-interview/master/posters/Bloodshot.jpg',
-        gender: 'Action',
+        genre: 'Action',
         description:
             'Ray Garrison, a slain soldier, is re-animated with superpowers.',
     },
@@ -65,7 +65,7 @@ const Movies: Array<Movie> = [
         name: 'Doctor Strange',
         poster:
             'https://raw.githubusercontent.com/enahum/mattermost-react-native-interview/master/posters/Doctor-Strange.jpg',
-        gender: 'Action',
+        genre: 'Action',
         description:
             'While on a journey of physical and spiritual healing, a brilliant neurosurgeon is drawn into the world of the mystic arts.',
     },
@@ -73,7 +73,7 @@ const Movies: Array<Movie> = [
         name: 'Extraction',
         poster:
             'https://raw.githubusercontent.com/enahum/mattermost-react-native-interview/master/posters/Extraction.jpg',
-        gender: 'Action',
+        genre: 'Action',
         description:
             "Tyler Rake, a fearless black market mercenary, embarks on the most deadly extraction of his career when he's enlisted to rescue the kidnapped son of an imprisoned international crime lord.",
     },
@@ -81,7 +81,7 @@ const Movies: Array<Movie> = [
         name: 'The Hangover',
         poster:
             'https://raw.githubusercontent.com/enahum/mattermost-react-native-interview/master/posters/Hangover.jpg',
-        gender: 'Comedy',
+        genre: 'Comedy',
         description:
             'Three buddies wake up from a bachelor party in Las Vegas, with no memory of the previous night and the bachelor missing. They make their way around the city in order to find their friend before his wedding.',
     },
@@ -89,7 +89,7 @@ const Movies: Array<Movie> = [
         name: 'Inception',
         poster:
             'https://raw.githubusercontent.com/enahum/mattermost-react-native-interview/master/posters/Inception.jpg',
-        gender: 'Action',
+        genre: 'Action',
         description:
             'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.',
     },
@@ -97,7 +97,7 @@ const Movies: Array<Movie> = [
         name: 'Kin',
         poster:
             'https://raw.githubusercontent.com/enahum/mattermost-react-native-interview/master/posters/Kin.jpg',
-        gender: 'Action',
+        genre: 'Action',
         description:
             'Chased by a vengeful criminal, the feds and a gang of otherworldly soldiers, a recently released ex-con, and his adopted teenage brother are forced to go on the run with a weapon of mysterious origin as their only protection.',
     },
@@ -105,7 +105,7 @@ const Movies: Array<Movie> = [
         name: 'The Lion King',
         poster:
             'https://raw.githubusercontent.com/enahum/mattermost-react-native-interview/master/posters/Lion-King.jpg',
-        gender: 'Animation',
+        genre: 'Animation',
         description:
             'Lion prince Simba and his father are targeted by his bitter uncle, who wants to ascend the throne himself.',
     },
@@ -113,7 +113,7 @@ const Movies: Array<Movie> = [
         name: 'The Matrix',
         poster:
             'https://raw.githubusercontent.com/enahum/mattermost-react-native-interview/master/posters/Matrix.jpg',
-        gender: 'Sci-fi',
+        genre: 'Sci-fi',
         description:
             'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.',
     },
@@ -121,7 +121,7 @@ const Movies: Array<Movie> = [
         name: 'Pirates of the Caribbean: On Stranger Tides',
         poster:
             'https://raw.githubusercontent.com/enahum/mattermost-react-native-interview/master/posters/Pirates-Caribbean.jpg',
-        gender: 'Adventure',
+        genre: 'Adventure',
         description:
             'Jack Sparrow and Barbossa embark on a quest to find the elusive fountain of youth, only to discover that Blackbeard and his daughter are after it too.',
     },
@@ -129,7 +129,7 @@ const Movies: Array<Movie> = [
         name: 'Rambo: First Blood',
         poster:
             'https://raw.githubusercontent.com/enahum/mattermost-react-native-interview/master/posters/Rambo.jpg',
-        gender: 'Action',
+        genre: 'Action',
         description:
             'A veteran Green Beret is forced by a cruel Sheriff and his deputies to flee into the mountains and wage an escalating one-man war against his pursuers.',
     },
@@ -137,7 +137,7 @@ const Movies: Array<Movie> = [
         name: 'Spies in Disguise',
         poster:
             'https://raw.githubusercontent.com/enahum/mattermost-react-native-interview/master/posters/Spies.jpg',
-        gender: 'Animation',
+        genre: 'Animation',
         description:
             "When the world's best spy is turned into a pigeon, he must rely on his nerdy tech officer to save the world.",
     },
@@ -145,7 +145,7 @@ const Movies: Array<Movie> = [
         name: 'Togo',
         poster:
             'https://raw.githubusercontent.com/enahum/mattermost-react-native-interview/master/posters/Togo.jpg',
-        gender: 'Biography',
+        genre: 'Biography',
         description:
             'The story of Togo, the sled dog who led the 1925 serum run yet was considered by most to be too small and weak to lead such an intense race.',
     },
@@ -153,7 +153,7 @@ const Movies: Array<Movie> = [
         name: 'Tron: Legacy',
         poster:
             'https://raw.githubusercontent.com/enahum/mattermost-react-native-interview/master/posters/Tron.jpg',
-        gender: 'Sci-Fi',
+        genre: 'Sci-Fi',
         description:
             "The son of a virtual world designer goes looking for his father and ends up inside the digital world that his father designed. He meets his father's corrupted creation and a unique ally who was born inside the digital world.",
     },
@@ -161,7 +161,7 @@ const Movies: Array<Movie> = [
         name: 'Up',
         poster:
             'https://raw.githubusercontent.com/enahum/mattermost-react-native-interview/master/posters/Up.jpg',
-        gender: 'Animation',
+        genre: 'Animation',
         description:
             '78-year-old Carl Fredricksen travels to Paradise Falls in his house equipped with balloons, inadvertently taking a young stowaway.',
     },
